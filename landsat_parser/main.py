0bdc9ff41fca689e181f63b3b45ffcd9426542f5
Jagraj Aulakh <aulakhjagraj@gmail.com>
2024-10-05 20:45:07 -0400

    Refactor predict_passover and load_sat_data

--- landsat_parser/main.py
+++ landsat_parser/main.py
@@ -1,36 +1,66 @@
+from collections import defaultdict
 from datetime import timedelta
 import json
+from typing import List
+
+from skyfield.iokit import os
 from landsatxplore.api import API
-from skyfield.api import Topos, load
+from skyfield.api import EarthSatellite, Time, Topos, load
+from pprint import pprint
 
-# Load the TLE file
-satellites = load.tle_file("../data/landsat8.tle.txt")
+satellites: List[EarthSatellite] = []
 
-# Find Landsat 8 satellite
-landsat8 = [sat for sat in satellites if "LANDSAT 8" in sat.name][0]
 
-print(f"Landsat 8: {landsat8}")
+def load_sat_data(filename: str) -> True:
+    # Load the TLE file
+    satFile = load.tle_file(filename)
+
+    # Find Landsat 8 and 9 satellites
+    landsat8 = [sat for sat in satFile if "LANDSAT 8" in sat.name][0]
+    landsat9 = [sat for sat in satFile if "LANDSAT 9" in sat.name][0]
+    satellites.append(landsat8)
+    satellites.append(landsat9)
+    return True
+
+
+def predict_passover(lat: float, lon: float, start_time: Time, end_time: Time):
+    # Holds all the dates
+    results = defaultdict(list)
+
+    print(enumerate(["joe", "mama"]))
+    for sat in satellites:
+        location = Topos(latitude_degrees=lat, longitude_degrees=lon)
+
+        # Predict passes over the location
+        t0, events = sat.find_events(
+            location, start_time, end_time, altitude_degrees=80
+        )
+
+        # Display the next pass details
+        for ti, event in zip(t0, events):
+            event_time = ti.utc_iso()  # ti is a Time object, we call utc_iso() on it
+            event_name = ["rise", "culmination", "set"][event]
+            if event == 0:
+                results[sat.name].append([event_time])
+            print(f"Event: {sat.name} {event_name} at {event_time}")
+    return results
+
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(curdir, "data/tle.txt")
+load_sat_data(filename)
 
 lat = 42.18856439825814
 lon = -82.82869985045305
-location = Topos(latitude_degrees=lat, longitude_degrees=lon)
 
 # Load timescale
 ts = load.timescale()
+# start_time = ts.now()
+# end_time = ts.utc(ts.now().utc_datetime() + timedelta(days=2))
+start_time = ts.utc(ts.now().utc_datetime() - timedelta(days=35))
+end_time = ts.now()
 
-# Calculate next 10 days
-time_now = ts.now()
-time_limit = ts.utc(time_now.utc_datetime() + timedelta(days=2))
-
-# Predict passes over the location
-t0, events = landsat8.find_events(location, time_now, time_limit, altitude_degrees=30.0)
-
-# Display the next pass details
-for ti, event in zip(t0, events):
-    event_time = ti.utc_iso()  # ti is a Time object, we call utc_iso() on it
-    event_name = ["rise", "culmination", "set"][event]
-    print(f"Event: {event_name} at {event_time}")
-
+print(predict_passover(lat, lon, start_time, end_time))
 
 # Define your EarthExplorer credentials
 username = "spaceapps43"
@@ -44,11 +74,12 @@ lat = 42.59113799085078
 lon = -83.19858770096863
 
 scenes = api.search(
-    dataset="landsat_ot_c2_l1",
+    dataset="landsat_ot_c2_l2",
     latitude=lat,
     longitude=lon,
     start_date="2024-09-01",
     end_date="2024-10-18",
+    max_cloud_cover=20,
 )
 
 print(f"{len(scenes)} found")
@@ -56,7 +87,8 @@ print(f"{len(scenes)} found")
 for scene in scenes:
     print(scene['acquisition_date'].strftime('%Y-%m-%d'))
     # Write scene footprints to disk
-    fname = f"{scene['landsat_product_id']}.geojson"
+    fname = f"{scene['display_id']}.geojson"
+    print(scene)
     with open(fname, "w") as f:
         json.dump(scene['spatial_coverage'].__geo_interface__, f)
 
