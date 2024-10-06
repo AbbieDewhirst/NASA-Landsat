90ec7c6294cad26e88014b081c05469cde47ee69
Jagraj Aulakh <aulakhjagraj@gmail.com>
2024-10-06 12:07:15 -0400

    Use preduction function from parser module

--- landsat_webapp/app.py
+++ landsat_webapp/app.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
 from datetime import timedelta
-from typing import List
 
 from flask import Flask, render_template, request, redirect, flash, jsonify
 import os
@@ -10,8 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, current_user, logout_user, login_user
 import bcrypt
 from skyfield.api import load
-from skyfield.sgp4lib import EarthSatellite
-from skyfield.toposlib import Topos
+
+from landsat_parser.main import load_sat_data, predict_passover
 
 load_dotenv()
 
@@ -39,20 +37,7 @@ class User(db.Model, UserMixin):
 
 
 # Load satellite data at app startup
-satellites: List[EarthSatellite] = []
 
-def load_sat_data(filename: str) -> True:
-    sat_file = load.tle_file(filename)
-    landsat8 = [sat for sat in sat_file if "LANDSAT 8" in sat.name][0]
-    landsat9 = [sat for sat in sat_file if "LANDSAT 9" in sat.name][0]
-    satellites.append(landsat8)
-    satellites.append(landsat9)
-    return True
-
-# Load the satellite data
-curdir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(curdir, "data/tle.txt")
-load_sat_data(filename)
 
 # Home route for selecting location
 @app.get("/")
@@ -112,53 +97,28 @@ from skyfield.api import Topos, load
 
 @app.get("/predict")
 def predict_passover_endpoint():
+    # Load the satellite data
+    curdir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(curdir, "data/tle.txt")
+    satellites = load_sat_data(filename)
+
     # Get lat and lon from the request
-    lat = float(request.args.get('lat', 0))
-    lon = float(request.args.get('lon', 0))
+    lat = float(request.args.get("lat", 0))
+    lon = float(request.args.get("lon", 0))
+    days = int(request.args.get("days", 30))
 
     # Load timescale
     ts = load.timescale()
 
     # Set the initial search range (30 days)
-    days_increment = 30
     start_time = ts.now()
-    end_time = ts.utc(ts.now().utc_datetime() + timedelta(days=days_increment))
+    end_time = ts.utc(ts.now().utc_datetime() + timedelta(days=days))
 
-    # Store the first occurrence of passovers for each satellite
-    first_occurrence = {"LANDSAT 8": None, "LANDSAT 9": None}
-
-    # Keep querying in 30-day increments until both LANDSAT 8 and LANDSAT 9 have results
-    while not (first_occurrence["LANDSAT 8"] and first_occurrence["LANDSAT 9"]):
-        results = defaultdict(list)
-
-        for sat in satellites:
-            location = Topos(latitude_degrees=lat, longitude_degrees=lon)
-
-            # Predict passes over the location
-            t0, events = sat.find_events(
-                location, start_time, end_time, altitude_degrees=80
-            )
-
-            # Record the passover details
-            for ti, event in zip(t0, events):
-                event_time = ti.utc_iso()  # ti is a Time object, we call utc_iso() on it
-                if event == 0:  # We're only interested in the 'rise' event
-                    results[sat.name].append(event_time)
-
-        # Check for the first occurrence of both satellites
-        if not first_occurrence["LANDSAT 8"] and results["LANDSAT 8"]:
-            first_occurrence["LANDSAT 8"] = results["LANDSAT 8"][0]
-
-        if not first_occurrence["LANDSAT 9"] and results["LANDSAT 9"]:
-            first_occurrence["LANDSAT 9"] = results["LANDSAT 9"][0]
-
-        # If one of the satellites is missing, extend the time window
-        if not (first_occurrence["LANDSAT 8"] and first_occurrence["LANDSAT 9"]):
-            start_time = end_time  # Move the start time forward
-            end_time = ts.utc(end_time.utc_datetime() + timedelta(days=days_increment))  # Extend the end time
+    # Store the passovers for each satellite
+    prediction_results = predict_passover(lat, lon, start_time, end_time)
 
     # Return the first occurrence of passovers for both satellites
-    return jsonify(first_occurrence)
+    return jsonify(prediction_results)
 
 
 if __name__ == "__main__":
@@ -166,5 +126,4 @@ if __name__ == "__main__":
     if not os.path.exists("instance/site.db"):
         with app.app_context():
             db.create_all()
-    app.run(debug=True, host='0.0.0.0')
-
+    app.run(debug=True, host="0.0.0.0")
